ee5c146b6bfb4da35688daec124eb385fc7d32d1
LianNidam <liannidam04@gmail.com>
2022-03-13 18:07:36 +0200

    Effect of pressing a button and playing sound when pressed

--- game.js
+++ game.js
@@ -1,21 +1,36 @@
+var buttonColor = ["red", "blue", "green", "yellow"];
 
-var buttonColor= ["red","blue", "green", "yellow"];
-var gamePattern= [];
+var gamePattern = [];
+var userClickedPattern = [];
 
-var userClickedPattern= [];
-
- function nextSequence(){
-   var randomNumber= Math.floor(Math.random()*4);
-   var randomChosenColour= buttonColor[randomNumber];
-   gamePattern.push(randomChosenColour);
-
-   $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-   var audio = new Audio("sounds/"+ randomChosenColour+ ".mp3");
-
- }
-
-$(".btn").click(function(){
-  var userChosenColour= $(this).attr("id");
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColor[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
